2e4d0a0d4ced9ce85c545b5e9653532f5c98b56e
alexandercavin <alexander.cavin2@gmail.com>
2024-11-12 16:12:43 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { AppBar, Toolbar, Typography, Container, Card, CardMedia, CardContent, Box, CssBaseline } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <CssBaseline />
+
+
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            React
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      
+      <Container maxWidth="sm" sx={{ mt: 4, mb: 4, flexGrow: 1 }}>
+        <Typography variant="h4" component="h1" gutterBottom align="center">
+          That feeling when knee surgery is tomorrow 
+        </Typography>
+
+    
+        <Card sx={{ mt: 4 }}>
+          <CardMedia
+            component="img"
+            image="https://storage.googleapis.com/proyekdeployaplikasiprofile/kneesurgery.png"
+            alt="Sample Image"
+            sx={{ objectFit: 'contain' }} 
+          />
+          <CardContent>
+            <Typography variant="body1" color="text.secondary">
+              BangkitBangkitBangkitBangkitBangkitBangkitBangkitBangkit.
+            </Typography>
+          </CardContent>
+        </Card>
+      </Container>
+
+      {/* Footer */}
+      <Box component="footer" sx={{ py: 3, backgroundColor: 'primary.main', color: 'white', textAlign: 'center' }}>
+        <Typography variant="body2">
+          © 2024 My Simple React Site
+        </Typography>
+      </Box>
+    </Box>
   );
 }
 
